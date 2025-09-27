366bef0a7be5d11cdce5a16c99e6fea3e2eda6ab
mohamedyasser <yasoesr@gmail.com>
2025-09-27 17:30:29 +0300

    refactor ProcessRudderRequest.php; comment out database connection and queue initialization

--- app/Jobs/ProcessRudderRequest.php
+++ app/Jobs/ProcessRudderRequest.php
@@ -60,8 +60,7 @@ class ProcessRudderRequest implements ShouldQueue
         $this->data = $data;
         $this->headers = $headers;
         $this->path = $path;
-//        $this->onConnection('database');
-//        $this->onQueue('rudder');
+        $this->onQueue('high');
     }
 
     /**
