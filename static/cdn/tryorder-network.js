bb762219fb798d7af52b082ce37e8709d8fcdd14
mohamedyasser <yasoesr@gmail.com>
2025-09-30 13:02:26 +0300

    refine network logging; restrict XHR response logging to 'otp-login' requests and comment out fetch interceptor

--- static/cdn/tryorder-network.js
+++ static/cdn/tryorder-network.js
@@ -10,29 +10,31 @@
     XMLHttpRequest.prototype.send = function(body) {
         const { method, url } = this.__log || {}
         if (url && url.includes(URL_FILTER)) {
-            console.log("[XHR REQUEST]", method, url, "Body:", body)
-            this.addEventListener("load", () => {
-                console.log("[XHR RESPONSE]", method, url, "Status:", this.status, "Body:", this.responseText)
-            })
+            if (url.includes('otp-login')) {
+                this.addEventListener("load", () => {
+                    console.log("[XHR RESPONSE]", method, url, "Status:", this.status, "Body:", this.responseText)
+                })
+            }
+
         }
         return _send.call(this, body)
     }
 
-    if (window.fetch) {
-        const _fetch = window.fetch
-        window.fetch = async (...args) => {
-            const [input, init = {}] = args
-            const url = typeof input === "string" ? input : input.url
-            if (url && url.includes(URL_FILTER)) {
-                console.log("[FETCH REQUEST]", init.method || "GET", url, "Body:", init.body)
-                const res = await _fetch(...args)
-                const clone = res.clone()
-                const text = await clone.text().catch(() => "")
-                console.log("[FETCH RESPONSE]", init.method || "GET", url, "Status:", res.status, "Body:", text)
-                return res
-            }
-            return _fetch(...args)
-        }
-    }
-    console.log("Network logger active. Filtering by:", URL_FILTER)
+    // if (window.fetch) {
+    //     const _fetch = window.fetch
+    //     window.fetch = async (...args) => {
+    //         const [input, init = {}] = args
+    //         const url = typeof input === "string" ? input : input.url
+    //         if (url && url.includes(URL_FILTER)) {
+    //             console.log("[FETCH REQUEST]", init.method || "GET", url, "Body:", init.body)
+    //             const res = await _fetch(...args)
+    //             const clone = res.clone()
+    //             const text = await clone.text().catch(() => "")
+    //             console.log("[FETCH RESPONSE]", init.method || "GET", url, "Status:", res.status, "Body:", text)
+    //             return res
+    //         }
+    //         return _fetch(...args)
+    //     }
+    // }
+    // console.log("Network logger active. Filtering by:", URL_FILTER)
 })()
