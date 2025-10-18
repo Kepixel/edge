397ee6905733f573a37ed9c20a43761f2e1d8a52
mohamedyasser <yasoesr@gmail.com>
2025-10-18 12:44:35 +0300

    add UpdateController and route for plugin metadata endpoint

---
+++ app/Http/Controllers/UpdateController.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers;
+
+class UpdateController extends Controller
+{
+    public function pluginMetadata()
+    {
+
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,7 @@
 <?php
 
+use App\Http\Controllers\UpdateController;
+
 require __DIR__.'/edges.php';
+
+Route::get('/wordpress-plugin/metadata.json', [UpdateController::class, 'pluginMetadata']);
