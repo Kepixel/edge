98336c25d1b86ea0f3c785d326c01fea2675d25a
mohamedyasser <yasoesr@gmail.com>
2025-10-18 13:34:04 +0300

    add UpdateController and route for plugin metadata endpoint

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -6,8 +6,30 @@ class UpdateController extends Controller
 {
     public function pluginMetadata()
     {
-        return response()->json([
-            'name' => 'Anubis',
-        ]);
+        $version = '1.0.0';
+        $payload = [
+            'name'          => 'Kepixel',
+            'slug'          => 'kepixel',
+            'version'       => $version,
+            'download_url'  => 'https://cdn.kepixel.com/wordpress/plugin.zip?v=' . $version,
+            'requires'      => '6.0',
+            'tested'        => '6.6.2',
+            'requires_php'  => '7.4',
+            'last_updated'  => '2025-10-18 09:00:00',
+            'homepage'      => 'https://www.kepixel.com/',
+            'sections'      => [
+                'description' => 'Bug fixes and improvements',
+                'changelog'   => '1.0.0 Fix X. Improve Y.',
+            ],
+            'banners'       => [
+                'low'  => 'https://edge.kepixel.com/wordpress-plugin/banner-772x250.png',
+                'high' => 'https://edge.kepixel.com/wordpress-plugin/banner-1544x500.png',
+            ],
+        ];
+
+        return response()->json($payload, 200, [
+            'Cache-Control' => 'public, max-age=900',
+            'Access-Control-Allow-Origin' => '*',
+        ])->setEtag(sha1(json_encode($payload)));
     }
 }
