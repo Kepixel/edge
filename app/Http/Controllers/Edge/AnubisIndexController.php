ecb5b7be2d087f2136cde08bd2118dcb2295e1f0
mohamedyasser <yasoesr@gmail.com>
2025-11-22 16:24:55 +0200

    feat: add logging for event data handling in salla.js

--- app/Http/Controllers/Edge/AnubisIndexController.php
+++ app/Http/Controllers/Edge/AnubisIndexController.php
@@ -78,7 +78,9 @@ class AnubisIndexController extends Controller
 
         if ($sourceKey == '01KADKP3YX3BAW9GEG8XK1FVB7') {
             $ga = $source->destinations->where('platform', 'google-analytics-4')->first();
-            dd($ga);
+            if ($ga) {
+                dd($ga, $ga->config);
+            }
         }
 
 //        $ga = $source->destinations;
