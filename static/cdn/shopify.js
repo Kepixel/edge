cdf47f26c6f54eaec4b706279a4eb66ae605cb93
mohamedyasser <yasoesr@gmail.com>
2025-11-23 17:47:22 +0200

    feat: add Shopify integration and enhance GA event handling

--- static/cdn/shopify.js
+++ static/cdn/shopify.js
@@ -1,5 +1,5 @@
 (function() {
-    var getField = function (t, e) {
+    var init = {}, getField = function (t, e) {
         return e.split(".").reduce(function (t, e) {
             return t ? t[e] : void 0
         }, t)
@@ -34,44 +34,74 @@
 
     const handleProductViewed = (event) => {
         setUserTraits(event);
+        window.kepixelAnalytics.track('', {
+
+        });
+
         console.log('product_viewed', event);
     };
 
     const handleCollectionViewed = (event) => {
         setUserTraits(event);
+        window.kepixelAnalytics.track('', {
+
+        });
         console.log('collection_viewed', event);
     };
 
     const handleSearchSubmitted = (event) => {
         setUserTraits(event);
+        window.kepixelAnalytics.track('', {
+
+        });
         console.log('search_submitted', event);
     };
 
     const handleCartViewed = (event) => {
         setUserTraits(event);
+        window.kepixelAnalytics.track('', {
+
+        });
         console.log('cart_viewed', event);
     };
 
     const handleProductAddedToCart = (event) => {
         setUserTraits(event);
+        window.kepixelAnalytics.track('', {
+
+        });
         console.log('product_added_to_cart', event);
     };
 
     const handleProductRemovedFromCart = (event) => {
         setUserTraits(event);
+        window.kepixelAnalytics.track('', {
+
+        });
         console.log('product_removed_from_cart', event);
     };
 
     const handlePaymentInfoSubmitted = (event) => {
+        setUserTraits(event);
+        window.kepixelAnalytics.track('', {
+
+        });
         console.log('payment_info_submitted', event);
     };
 
     const handleCheckoutStarted = (event) => {
+        setUserTraits(event);
+        window.kepixelAnalytics.track('', {
+
+        });
         console.log('checkout_started', event);
     };
 
     const handleCheckoutCompleted = (event) => {
         setUserTraits(event);
+        window.kepixelAnalytics.track('', {
+
+        });
         console.log('checkout_completed', event);
     };
 
