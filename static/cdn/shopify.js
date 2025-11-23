025e64948409e1957634fbb3cf670fe665bc2cc2
mohamedyasser <yasoesr@gmail.com>
2025-11-23 17:43:09 +0200

    feat: add Shopify integration and enhance GA event handling

--- static/cdn/shopify.js
+++ static/cdn/shopify.js
@@ -1,29 +1,64 @@
 (function() {
+    var getField = function (t, e) {
+        return e.split(".").reduce(function (t, e) {
+            return t ? t[e] : void 0
+        }, t)
+    }, addUserAddress = function (t, e) {
+        userTraits.address = t && t.address1 || e && e.address1, userTraits.city = t && t.city || e && e.city, userTraits.country = t && t.country || e && e.country, userTraits.country_code = t && t.countryCode || e && e.countryCode, userTraits.state = t && t.province || e && e.province, userTraits.state_code = t && t.provinceCode || e && e.provinceCode, userTraits.postal_code = t && t.zip || e && e.zip
+    }, setUserTraits = function (t) {
+        let e = {};
+        t && t.data && (e = t.data), userTraits.userId = getField(t, "clientId");
+        let a = getField(e, "checkout.billingAddress.email"), i = getField(e, "checkout.shippingAddress.email"),
+            r = getField(e, "checkout.email"), c = getField(init, "data.customer.email");
+        userTraits.email = c || r || i || a || "";
+        let n = getField(e, "checkout.billingAddress.phone"), d = getField(e, "checkout.shippingAddress.phone"),
+            o = getField(e, "checkout.phone"), s = getField(init, "data.customer.phone");
+        userTraits.phone = s || o || d || n || "";
+        let u = getField(e, "checkout.billingAddress.firstName"),
+            l = getField(e, "checkout.shippingAddress.firstName"), m = getField(e, "checkout.firstName"),
+            p = getField(init, "data.customer.firstName");
+        userTraits.firstname = p || m || l || u || "";
+        let y = getField(e, "checkout.billingAddress.lastName"),
+            _ = getField(e, "checkout.shippingAddress.lastName"), h = getField(e, "checkout.lastName"),
+            v = getField(init, "data.customer.lastName");
+        userTraits.lastname = v || h || _ || y || "", e && e.checkout && addUserAddress(e.checkout.billingAddress, e.checkout.shippingAddress)
+    }, userTraits = {};
+
+
+    setUserTraits();
+
     const handlePageViewed = (event) => {
+        setUserTraits(event);
         window.kepixelAnalytics.page();
     };
 
     const handleProductViewed = (event) => {
+        setUserTraits(event);
         console.log('product_viewed', event);
     };
 
     const handleCollectionViewed = (event) => {
+        setUserTraits(event);
         console.log('collection_viewed', event);
     };
 
     const handleSearchSubmitted = (event) => {
+        setUserTraits(event);
         console.log('search_submitted', event);
     };
 
     const handleCartViewed = (event) => {
+        setUserTraits(event);
         console.log('cart_viewed', event);
     };
 
     const handleProductAddedToCart = (event) => {
+        setUserTraits(event);
         console.log('product_added_to_cart', event);
     };
 
     const handleProductRemovedFromCart = (event) => {
+        setUserTraits(event);
         console.log('product_removed_from_cart', event);
     };
 
@@ -36,6 +71,7 @@
     };
 
     const handleCheckoutCompleted = (event) => {
+        setUserTraits(event);
         console.log('checkout_completed', event);
     };
 
