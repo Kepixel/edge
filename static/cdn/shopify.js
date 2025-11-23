c0d659cbf0983503fafd805929b1506f463d9a1c
mohamedyasser <yasoesr@gmail.com>
2025-11-23 16:57:40 +0200

    feat: add Shopify integration and enhance GA event handling

--- static/cdn/shopify.js
+++ static/cdn/shopify.js
@@ -1,30 +1,56 @@
-analytics.subscribe("page_viewed", t => {
-    console.log('page_viewed')
-});
-analytics.subscribe("product_viewed", t => {
-    console.log('product_viewed')
-});
-analytics.subscribe("collection_viewed", t => {
-    console.log('collection_viewed')
-});
-analytics.subscribe("search_submitted", t => {
-    console.log('search_submitted')
-});
-analytics.subscribe("cart_viewed", t => {
-    console.log('cart_viewed')
-});
-analytics.subscribe("product_added_to_cart", t => {
-    console.log('product_added_to_cart')
-});
-analytics.subscribe("product_removed_from_cart", t => {
-    console.log('product_removed_from_cart')
-});
-analytics.subscribe("payment_info_submitted", t => {
-    console.log('payment_info_submitted')
-});
-analytics.subscribe("checkout_started", t => {
-    console.log('checkout_started')
-});
-analytics.subscribe("checkout_completed", t => {
-    console.log('checkout_completed')
-});
+(function() {
+    const handlePageViewed = (event) => {
+        console.log('page_viewed', event);
+    };
+
+    const handleProductViewed = (event) => {
+        console.log('product_viewed', event);
+    };
+
+    const handleCollectionViewed = (event) => {
+        console.log('collection_viewed', event);
+    };
+
+    const handleSearchSubmitted = (event) => {
+        console.log('search_submitted', event);
+    };
+
+    const handleCartViewed = (event) => {
+        console.log('cart_viewed', event);
+    };
+
+    const handleProductAddedToCart = (event) => {
+        console.log('product_added_to_cart', event);
+    };
+
+    const handleProductRemovedFromCart = (event) => {
+        console.log('product_removed_from_cart', event);
+    };
+
+    const handlePaymentInfoSubmitted = (event) => {
+        console.log('payment_info_submitted', event);
+    };
+
+    const handleCheckoutStarted = (event) => {
+        console.log('checkout_started', event);
+    };
+
+    const handleCheckoutCompleted = (event) => {
+        console.log('checkout_completed', event);
+    };
+
+    // Expose handlers globally
+    window.kepixel = {
+        isReady: true,
+        handlePageViewed,
+        handleProductViewed,
+        handleCollectionViewed,
+        handleSearchSubmitted,
+        handleCartViewed,
+        handleProductAddedToCart,
+        handleProductRemovedFromCart,
+        handlePaymentInfoSubmitted,
+        handleCheckoutStarted,
+        handleCheckoutCompleted
+    };
+})();
