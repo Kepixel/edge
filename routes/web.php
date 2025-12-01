75e7c8ea37105e677b82f56b28c65fdc86cee7f5
mohamedyasser <yasoesr@gmail.com>
2025-12-01 17:25:26 +0200

    feat: add route for CSV export of events by source key

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,10 @@
 
 use App\Http\Controllers\UpdateController;
 
+Route::get('events/{sourceKey}.csv', function (string $sourceKey) {
+
+});
+
 Route::get('/wordpress-plugin/metadata.json', [UpdateController::class, 'pluginMetadata']);
 
 require __DIR__.'/edges.php';
