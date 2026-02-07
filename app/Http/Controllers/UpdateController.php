d3d1da915401720d37e7ac77169f6b4dfefa22d1
mohamedyasser <yasoesr@gmail.com>
2026-02-07 07:05:44 +0200

    Bump plugin version to v1.0.13

--- app/Http/Controllers/UpdateController.php
+++ app/Http/Controllers/UpdateController.php
@@ -6,7 +6,7 @@ class UpdateController extends Controller
 {
     public function pluginMetadata()
     {
-        $version = '1.0.12';
+        $version = '1.0.13';
         $payload = [
             'name'          => 'Kepixel',
             'slug'          => 'kepixel',
@@ -15,7 +15,7 @@ class UpdateController extends Controller
             'requires'      => '6.0',
             'tested'        => '6.6.2',
             'requires_php'  => '7.4',
-            'last_updated'  => '2026-02-07 05:00:28',
+            'last_updated'  => '2026-02-07 05:05:05',
             'homepage'      => 'https://www.kepixel.com/',
             'sections'      => [
                 'description' => 'Bug fixes and improvements',
